2c7706fda703646037c87d6960889d4d821f0df4
arturbaccarin <baccarin.artur@gmail.com>
2024-05-08 19:13:31 -0300

    using @autowired

--- algafood-api/src/main/java/com/algaworks/algafoodapi/di/notification/EmailNotificator.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/di/notification/EmailNotificator.java
@@ -1,31 +1,15 @@
 package com.algaworks.algafoodapi.di.notification;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafoodapi.di.model.Client;
 
+@Component
 public class EmailNotificator implements Notificator {
-
-    private boolean upperCase;
-    private String smtpHostServer;
-
-    public EmailNotificator(String smtpHostServer) {
-        this.smtpHostServer = smtpHostServer;
-        System.out.println("EmailNotificator");
-    }
 
     @Override
     public void notificate(Client client, String message) {
-        if (this.upperCase) {
-            message = message.toUpperCase();
-        }
-
-        System.out.printf("Notifying %s in e-mail %s using SMTP %s: %s\n",
-                client.getName(),
-                client.getEmail(),
-                this.smtpHostServer,
-                message);
-    }
-
-    public void setUpperCase(boolean upperCase) {
-        this.upperCase = upperCase;
+        System.out.printf("Notifying %s in e-mail %s using SMTP: %s\n",
+                client.getName(), client.getEmail(), message);
     }
 }
--- algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/ClientActivationService.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/ClientActivationService.java
@@ -1,20 +1,34 @@
 package com.algaworks.algafoodapi.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafoodapi.di.model.Client;
 import com.algaworks.algafoodapi.di.notification.Notificator;
 
+@Component
 public class ClientActivationService {
 
+    @Autowired
     private Notificator notificator;
 
-    public ClientActivationService(Notificator notificator) {
-        this.notificator = notificator;
-        System.out.println("ClientActivationService: " + notificator);
-    }
+    // @Autowired
+    // public ClientActivationService(Notificator notificator) {
+    // this.notificator = notificator;
+    // }
+
+    // public ClientActivationService(String s) {
+
+    // }
 
     public void activate(Client client) {
         client.active();
 
         this.notificator.notificate(client, "Your register is active!");
     }
+
+    // @Autowired
+    // public void setNotificator(Notificator notificator) {
+    // this.notificator = notificator;
+    // }
 }
