9d9a2306e2b087c6611cf05f317ba69a6d391f58
arturbaccarin <baccarin.artur@gmail.com>
2024-05-23 14:08:49 -0300

    custom properties

--- algafood-api/src/main/java/com/algaworks/algafoodapi/di/notification/EmailNotificator.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/di/notification/EmailNotificator.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafoodapi.di.notification;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,21 @@ import com.algaworks.algafoodapi.di.model.Client;
 @Component
 public class EmailNotificator implements Notificator {
 
+    @Value("${notificator.email.host-server}")
+    private String host;
+
+    @Value("${notificator.email.port-server}")
+    private Integer port;
+
     public EmailNotificator() {
         System.out.println("Real EmailNotificator");
     }
 
     @Override
     public void notificate(Client client, String message) {
+        System.out.println("Host: " + host);
+        System.out.println("Port: " + port);
+
         System.out.printf("Notifying %s in e-mail %s using SMTP: %s\n",
                 client.getName(), client.getEmail(), message);
     }
