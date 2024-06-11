326fcdca564ce3c3945df36255b83a2d6626ccf1
arturbaccarin <baccarin.artur@gmail.com>
2024-06-11 19:01:25 -0300

    ESR - 03.16

---
+++ introduction-to-jpa-and-hibernate/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Restaurante> listar() {
+        TypedQuery<Restaurante> query = manager.createQuery("from Restaurante", Restaurante.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public Restaurante buscar(Long id) {
+        return manager.find(Restaurante.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Restaurante salvar(Restaurante cozinha) {
+        return manager.merge(cozinha);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Restaurante cozinha) {
+        cozinha = buscar(cozinha.getId());
+        manager.remove(cozinha);
+    }
+
+}
--- introduction-to-jpa-and-hibernate/algafood-api/src/main/resources/import.sql
+++ introduction-to-jpa-and-hibernate/algafood-api/src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO cozinha (nome) values ('Tailandesa');
 INSERT INTO cozinha (nome) values ('Indiana');
+
+INSERT INTO restaurante(nome, taxa_frete) values ('Beco', 10.99)
+INSERT INTO restaurante(nome, taxa_frete) values ('Alfredo', 5.99)
